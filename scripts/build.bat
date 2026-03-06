ec323b1f98b93a9e54e15c7f89432182633e61c1
Jnyandeep <thokchom.jnyandeep@gmail.com>
2026-03-06 23:04:49 +0530

    try: trying dockerizing

---
+++ scripts/build.bat
@@ -0,0 +1,11 @@
+@echo off
+
+mkdir ..\bin
+
+echo [build] - Building evm-pmpc-node
+go build -o ..\bin\evm-pmpc-node.exe cmd\node\main.go
+echo [build] - Done building evm-pmpc-node
+
+echo [build] - Building evm-pmpc-bootstrap
+go build -o ..\bin\evm-pmpc-bootstrap.exe cmd\bootstrapnode\main.go
+echo [build] - Done building evm-pmpc-bootstrap
